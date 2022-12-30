f9af2f84572aa300426879f542b7de7e534ee731
Garrett Wininger <garrett.s.wininger@outlook.com>
2022-12-30 18:11:44 -0500

    Integration testing for C/C++

---
+++ test/integration/c_cpp/controls/gcc.rb
@@ -0,0 +1,15 @@
+control 'GCC/G++-Installation' do
+    title 'Validate that GCC/G++ are installed'
+
+    describe package('gcc') do
+        it {should be_installed}
+    end
+
+    describe package('g++') do
+        it {should be_installed}
+    end
+
+    describe package('gdb') do
+        it {should be_installed}
+    end
+end
